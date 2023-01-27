2711a53a7c2fce9e489eb1b8c301c28e1f2fd9b4
Spencer Pilutti <spencerpilutti@yahoo.com>
2023-01-27 17:53:14 -0600

    fixed annoying placement

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ function changeOpaOut() {
     element[0].classList.toggle("transformed-state-out");
 }
 
+function deleteFade() {
+    element[0].remove();
+}
+
 element[0].addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         changeOpaOut();
+        setTimeout(deleteFade, 3000)
     };
 });
+
 
 
 window.onload = changeOpaIn()
