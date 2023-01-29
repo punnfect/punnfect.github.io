92d143510f590932f28a9009f6d78f0839da02de
Spencer Pilutti <spencerpilutti@yahoo.com>
2023-01-28 18:09:20 -0600

    added proper fade out/in

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-let element = document.getElementsByClassName("fadeIn")
+let element = document.getElementsByClassName("fadeIn");
+let change = document.getElementById("change");
+let inputText = document.getElementById("ask-name");
+let projects = document.getElementsByClassName("project-container");
+let personName;
 
 function changeOpaIn() {
     //let element = document.getElementsByClassName("fadeIn")
-    element[0].classList.toggle("transformed-state-in");
+    element[0].classList.add("transformed-state-in");
 }
 
 function changeOpaOut() {
-    element[0].classList.toggle("transformed-state-out");
+    element[0].classList.remove("transformed-state-in");
+}
+
+function changeProjOpa() {
+    projects[0].classList.add("transformed-state-in");
+    projects[1].classList.add("transformed-state-in");
+}
+
+function changeReappear (personName) {
+    change.innerText = "Welcome to my website" + " " + personName;
+    element[0].classList.add("transformed-state-in");
+    setTimeout(changeOpaOut, 3000);
+    setTimeout(changeProjOpa, 4500);
 }
 
 function deleteFade() {
-    element[0].remove();
+    inputText.remove();
 }
 
 element[0].addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         changeOpaOut();
-        setTimeout(deleteFade, 3000)
+        personName = document.getElementById("ask-name").value;
+        deleteFade();
+        //change.innerText = personName;
+        //alert(personName)
+        setTimeout(function() {changeReappear(personName);}, 3500);
     };
 });
 
 
-
-window.onload = changeOpaIn()
+window.onload = changeOpaIn();
 
 // timeout function --- setTimeout(function i want to run, time in miliseconds ex 3000);
